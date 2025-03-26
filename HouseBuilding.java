3d9fad7ae06ba355765648f682063f8904ef666a
samriddhi-v-k <samriddhivk17@gmail.com>
2025-03-26 08:26:31 +0530

    adding assignment files

---
+++ HouseBuilding.java
@@ -0,0 +1,14 @@
+class HouseBuilding
+{
+	public static void main(String home[])
+	{
+		House contract= new House("tommy Villa","123 Main Street,bng",4,"Johnny"," Villa",2);
+		System.out.println(contract);
+		contract.own();
+	
+	
+	
+	}
+
+
+}
---
+++ PaperPrinting.java
@@ -0,0 +1,14 @@
+class PaperPrinting
+{
+public static void main(String paper[])
+{
+	Paper timesOfIndia= new Paper("20,000",30,5,"garfield","tanishq diameond collection","standard newsprint","Radia Tapes Scandal");
+	System.out.println(timesOfIndia);
+	timesOfIndia.publish();
+
+
+
+}
+
+
+}
