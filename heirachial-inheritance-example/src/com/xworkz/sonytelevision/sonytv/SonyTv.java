365c36963fd08850cf6500f1ddea3c3df4d481da
samriddhi-v-k <samriddhivk17@gmail.com>
2025-05-28 05:47:41 +0530

    Update SonyTv.java

--- heirachial-inheritance-example/src/com/xworkz/sonytelevision/sonytv/SonyTv.java
+++ heirachial-inheritance-example/src/com/xworkz/sonytelevision/sonytv/SonyTv.java
@@ -2,6 +2,6 @@ package com.xworkz.sonytelevision.sonytv;
 
 public class SonyTv {
     public void processor(){
-        System.out.println("it has an expensive[over budget],but known as intelligent brain");
+        System.out.println("it is expensive");
     }
 }
